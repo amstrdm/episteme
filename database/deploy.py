5509a830dc7685c72227d9a5d00d1c3e02cc0201
amstrdm <a.caserta@proton.me>
2024-12-06 07:30:15 +0100

    added create_database_if_not_exists to deployment script

---
+++ database/deploy.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, text
+
+def create_database_if_not_exists(user, password, host, dbname):
+    """Checks if database exists and creates one if it does not"""
+    engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}/postgres")
+
+    with engine.connect() as conn:
+        # Check if Database exists
+        result = conn.execute(text(f"SELECT 1 from pg_database WHERE datname='{dbname}'"))
+
+        if not result.scalar(): # No rows returned means the DB doesn't exist
+            conn.execute(text("COMMIT")) # PostgreSQL requires a Commit before creating a db
+            conn.execute(text(f"CREATE DATABASE {dbname}"))
+            print(f"Database {dbname} created successfully")
+        else:
+            print(f"Database {dbname} already exists")
