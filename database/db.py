5704f1eba62bbc71b8c63be0322ea819c494ada2
amstrdm <a.caserta@proton.me>
2024-12-08 17:10:06 +0100

    created db.py to set up database conection and session

---
+++ database/db.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tables import Base
+
+load_dotenv("../config/config.env")
+
+DB_USER = os.getenv("POSTGRESQL_USER")
+DB_PASSWORD = os.getenv("POSTGRESQL_PASSWORD")
+DB_HOST = os.getenv("POSTGRESQL_HOST")
+DB_NAME = os.getenv("POSTGRESQL_DBNAME")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+# Create the Engine and session factory
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create Tables if they don't exist
+Base.metadata.create_all(bind=engine)
