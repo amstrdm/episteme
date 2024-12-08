e928083a07e26c8afc83849d1121ff73d0057fca
amstrdm <a.caserta@proton.me>
2024-12-08 20:25:14 +0100

    added deployment script for the stock_index database

---
+++ deploy_stock_index_db.py
@@ -0,0 +1,55 @@
+from sqlalchemy import create_engine, Text, Column, Integer
+from sqlalchemy.exc import OperationalError
+from dotenv import load_dotenv
+import os
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.ext.declarative import declarative_base
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(BASE_DIR, "./config/config.env")
+
+load_dotenv(ENV_PATH)
+
+Base = declarative_base()
+
+
+def create_database_if_not_exists(user, password, host, dbname):
+    """Checks if database exists and creates one if it does not"""
+    engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}/{dbname}")
+
+    if not database_exists(engine.url):
+        create_database(engine.url)
+        print(f"Database {dbname} created successfully")
+    
+    else:
+        print(f"Database {dbname} exists already")
+    
+def create_tables(user, password, host, dbname):
+    """Creates tables defined in SQLAlchemy models if they don't already exist"""
+    db_url = f"postgresql+psycopg2://{user}:{password}@{host}/{dbname}"
+    engine = create_engine(db_url)
+
+    try:
+        Base.metadata.create_all(engine)
+        print("All Tables created successfully (if not already present)")
+    except Exception as e:
+        print(f"Error creating tables: {e}")    
+
+class Stock(Base):
+    __tablename__ = "stocks"
+    id = Column(Integer, primary_key=True)
+    ticker = Column(Text, nullable=False)
+    name = Column(Text, nullable=False)
+
+if __name__ == "__main__":
+    DB_USER = os.getenv("POSTGRESQL_USER")
+    DB_PASSWORD = os.getenv("POSTGRESQL_PASSWORD")
+    DB_HOST = os.getenv("POSTGRESQL_HOST")
+    DB_NAME = os.getenv("POSTGRESQL_STOCKS_DBNAME")
+
+    # Create Database if it doesn't exist
+    create_database_if_not_exists(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+
+    # Create Tables
+    create_tables(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+    
