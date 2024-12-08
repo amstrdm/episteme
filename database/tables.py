7f02068116381defe959ac738c0f4319d4e76c82
amstrdm <a.caserta@proton.me>
2024-12-08 17:09:30 +0100

    renamed create_tables to tables

---
+++ database/tables.py
@@ -0,0 +1,85 @@
+from sqlalchemy import create_engine, Text, Column, Integer, String, DateTime, Boolean, ForeignKey, CheckConstraint
+from sqlalchemy.orm import  relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class Ticker(Base):
+    __tablename__ = "tickers"
+    id = Column(Integer, primary_key=True)
+    symbol = Column(String(10), unique=True, nullable=False)
+    name = Column(String(100))
+    overall_sentiment_score = Column(
+        Integer,
+        CheckConstraint("overall_sentiment_score BETWEEN 1 and 100"),
+        nullable=True
+    )
+    last_analyzed = Column(DateTime)
+
+    # Relationships
+    posts = relationship("Post", back_populates="ticker", cascade="all, delete-orphan")
+    points = relationship("Point", back_populates="ticker", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"<Ticker(id{self.id}, symbol{self.symbol}', name='{self.name}')>"
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    ticker_id = Column(ForeignKey("tickers.id"), nullable=False)
+    source = Column(String(50), nullable=False)
+    title = Column(String(250))
+    link = Column(Text)
+
+    # Relationships 
+
+    ticker = relationship("Ticker", back_populates="posts")
+    points = relationship("Point", back_populates="post", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"<Post(id{self.id}, source='{self.source}', title='{self.title}')>"
+    
+class Point(Base):
+    __tablename__ = "points"
+    id = Column(Integer, primary_key=True)
+    ticker_id = Column(Integer, ForeignKey("tickers.id"), nullable=False)
+    post_id = Column(ForeignKey("posts.id"), nullable=False)
+    sentiment_score = Column(
+        Integer,
+        CheckConstraint("sentiment_score BETWEEN 1 and 100"),
+        nullable=False
+    )
+    text = Column(Text, nullable=False)
+    date_of_post = Column(DateTime)
+    criticism_exists = Column(Boolean, default=False)
+
+    # Relationships
+    ticker = relationship("Ticker", back_populates="points")
+    post = relationship("Post", back_populates="points")
+    criticisms = relationship("Criticism", back_populates="points", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"<Point(id={self.id}, sentiment_score={self.sentiment_score}, text='{self.text[:30]}...')>"
+        
+class Criticism(Base):
+    __tablename__ = "criticisms"
+    id = Column(Integer, primary_key=True)
+    point_id = Column(Integer, ForeignKey="points.id", nullable=False)
+    text = Column(Text, nullable=False)
+    date_posted = Column(DateTime)
+    validity_score = Column(
+        Integer,
+        CheckConstraint("validity_score BETWEEN 1 and 100"),
+        nullable=True           
+    )
+
+    # Relationships 
+
+    point = relationship("Point", back_populates="criticisms")
+
+    def __repr__(self):
+        return f"<Criticism(id={self.id}, valdiity_score={self.validity_score})>"
+
+
+
