b27a18a154400bb2c0d0f2cab3d62506874227b0
amstrdm <a.caserta@proton.me>
2024-12-14 12:47:43 +0100

    added pool_pre_ping to engine

--- database/db.py
+++ database/db.py
@@ -4,7 +4,7 @@ from models.thesisai import Base
 from config.database_url import DATABASE_URL
 
 # Create the Engine and session factory
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create Tables if they don't exist
