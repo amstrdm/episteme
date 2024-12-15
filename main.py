996b8bc52822050d93010eb500695e6ffba27382
amstrdm <a.caserta@proton.me>
2024-12-15 16:15:54 +0100

    added main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from routers import stock_query
+from routers import stock_query, analysis_router, add_data
 
 app = FastAPI()
 
 app.include_router(stock_query.router)
+app.include_router(analysis_router.router)
+app.include_router(add_data.router)
