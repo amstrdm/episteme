92779fe05916c582c7efd1a52bc3630c1eef1efb
amstrdm <a.caserta@proton.me>
2025-01-11 16:20:11 +0100

    made checking of stock in database case insensitive

--- routers/analysis/check_existing_analysis.py
+++ routers/analysis/check_existing_analysis.py
@@ -1,9 +1,9 @@
 from database.db import SessionLocal
 from database.models.thesisai import Ticker
-
+from sqlalchemy import func
 
 def check_ticker_in_database(ticker: str):
     session = SessionLocal()
 
-    result = session.query(Ticker.symbol, Ticker.last_analyzed).filter(Ticker.symbol == ticker).first()
+    result = session.query(Ticker.symbol, Ticker.last_analyzed).filter(func.lower(Ticker.symbol) == ticker.lower()).first()
     return result.last_analyzed if result else None
