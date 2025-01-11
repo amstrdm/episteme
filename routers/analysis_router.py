dee86b165889b5b5d8fba4b7f20f9e2728c9255a
amstrdm <a.caserta@proton.me>
2025-01-11 16:52:04 +0100

    removed pydantic validation and online_confirmed value

--- routers/analysis_router.py
+++ routers/analysis_router.py
@@ -6,12 +6,12 @@ router = APIRouter()
 class StockQuery(BaseModel):
     ticker: str
     title: str
-    online_confirmed: bool = False # Checking if User wants to access offline analysis
 
 @router.post("/generate-analysis")
-def create_analysis(stock: StockQuery):
-    last_analyzed = check_ticker_in_database(stock.ticker)
+def create_analysis(ticker: str, title: str):
+    last_analyzed = check_ticker_in_database(ticker)
+    
     if last_analyzed is not None:
         return({"message": f"Ticker exists in database and was last analyzed on {last_analyzed}"})
     else:
-        return({"message": f"Ticker {stock.ticker} was not yet analyzed"})
+        return({"message": f"Ticker {ticker} was not yet analyzed"})
