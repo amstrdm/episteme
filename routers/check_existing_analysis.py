db64d3b89d79e24b9043c22ca929a07d3bb3d9be
amstrdm <a.caserta@proton.me>
2025-03-13 07:30:42 +0100

    reworked checkin of existing analysis in database to return a tuple of if the ticker exists already and what the last analyzed date is

--- routers/check_existing_analysis.py
+++ routers/check_existing_analysis.py
@@ -8,14 +8,30 @@ router = APIRouter()
 def check_ticker_in_database(ticker: str):
     session = SessionLocal()
 
-    result = session.query(Ticker.symbol, Ticker.last_analyzed).filter(func.lower(Ticker.symbol) == ticker.lower()).first()
-    return result.last_analyzed if result else None
+    result = session.query(Ticker.last_analyzed).filter(func.lower(Ticker.symbol) == ticker.lower()).first()
 
+    if result is not None:
+        return True, result[0]
+    else:
+        return False, None
+    
 @router.get("/check-analysis")
 def create_analysis(ticker: str):
-    last_analyzed = check_ticker_in_database(ticker)
+    ticker_exists, last_analyzed = check_ticker_in_database(ticker)
     
-    if last_analyzed is not None:
-        return({"existing_analysis": True, "message": f"There is an existing analysis for '{ticker}' created on {last_analyzed}. Do you want to access it or create a new analysis?"})
+    if ticker_exists:
+        if last_analyzed is not None:
+            return {
+                "existing_analysis": True,
+                "message": f"There is an existing analysis for '{ticker}' created on {last_analyzed}. Do you want to access it or create a new analysis?"
+            }
+        else:
+            return {
+                "existing_analysis": True,
+                "message": f"'{ticker}' exists in the database, but no analysis has been done yet."
+            }
     else:
-        return({"existing_analysis": False, "message": f"{ticker} was not yet analyzed. Do you want to generate a new analysis for it?"})
+        return {
+            "existing_analysis": False,
+            "message": f"{ticker} was not found in the database. Do you want to generate a new analysis for it?"
+        }
