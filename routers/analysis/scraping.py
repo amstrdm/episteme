16fbe654a20960c49ab03fe3dfb187de30d78600
amstrdm <a.caserta@proton.me>
2025-04-04 18:50:41 +0200

    removed unnecessary try-except block

--- routers/analysis/scraping.py
+++ routers/analysis/scraping.py
@@ -9,32 +9,29 @@ def scrape_seekingalpha(ticker:str, seekingalpha_num_posts: int):
     return get_seekingalpha_posts_info(stock_ticker=ticker, num_posts=seekingalpha_num_posts)
 
 def scrape_content(ticker:str, title: str, subreddits, reddit_timeframe: str, reddit_num_posts: int, seekingalpha_num_posts):
-    try:
-        # Create a thread pool
-        with ThreadPoolExecutor(max_workers=2) as executor:
-            future_to_name = {
-                executor.submit(scrape_reddit, ticker, title, subreddits, reddit_timeframe, reddit_num_posts): "reddit",
-                executor.submit(scrape_seekingalpha, ticker, seekingalpha_num_posts): "seekingalpha"
-            }
+    # Create a thread pool
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        future_to_name = {
+            executor.submit(scrape_reddit, ticker, title, subreddits, reddit_timeframe, reddit_num_posts): "reddit",
+            executor.submit(scrape_seekingalpha, ticker, seekingalpha_num_posts): "seekingalpha"
+        }
 
-            # As they complete gather results
-            results = {}
-            for future in as_completed(future_to_name):
-                source_name = future_to_name[future]
-                try:
-                    data = future.result()
-                    results[source_name] = data
-                except Exception as e:
-                    # Log and re-raise with context
-                    raise RuntimeError(f"failed to scrape {source_name}:", e) from e
-        # 'results' now holds data from both threads
-        reddit_data = results.get("reddit", [])
-        seekingalpha_data = results.get("seekingalpha", [])
+        # As they complete gather results
+        results = {}
+        for future in as_completed(future_to_name):
+            source_name = future_to_name[future]
+            try:
+                data = future.result()
+                results[source_name] = data
+            except Exception as e:
+                # Log and re-raise with context
+                raise RuntimeError(f"failed to scrape {source_name}:", e) from e
+    # 'results' now holds data from both threads
+    reddit_data = results.get("reddit", [])
+    seekingalpha_data = results.get("seekingalpha", [])
 
 
-        return reddit_data + seekingalpha_data
-    except Exception as e:
-        raise RuntimeError(f"Scraping failed: {str(e)}") from e
+    return reddit_data + seekingalpha_data
 
 if __name__ == "__main__":
     scrape_content(ticker="VST", title="Vistra", subreddits=["stocks", "investing", "valueinvesting", "wallstreetbets"], reddit_timeframe="year", reddit_num_posts=1, seekingalpha_num_posts=1)
