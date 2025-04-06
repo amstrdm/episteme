16726b37189eced7a456024c52b25618f85dbf38
amstrdm <a.caserta@proton.me>
2025-04-06 03:38:36 +0200

    added CORS Middleware and added remote origins for dev and production with Production Origin set by env variable

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from routers import stock_query, create_analysis
 from routers import check_analysis_route
 from routers import return_db_contents
 from sqlalchemy.exc import SQLAlchemyError
 import logging
+from dotenv import load_dotenv
+import os
+
+ENV_PATH = os.getenv("ENV_PATH")
+load_dotenv(ENV_PATH)
+
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 logging.basicConfig(
     filename="errors.log", 
     encoding="utf-8",
     level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+if FRONTEND_URL:
+    origins.append(FRONTEND_URL)
 
 app = FastAPI()
 
@@ -30,6 +47,14 @@ async def generic_exception_handler(request: Request, exc: Exception):
         status_code=500,
         content={"detail": "An unexpected error occurred. Please try again later."}
     )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(stock_query.router)
 app.include_router(check_analysis_route.router)
