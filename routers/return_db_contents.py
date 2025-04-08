a59c8a6aed0518d7c3b8d92c773c88049f214325
amstrdm <a.caserta@proton.me>
2025-04-08 15:21:13 +0200

    added parameter only_database for analysis rerieval endpoint. Tells the backend to only fetch data from the database and avoid API calls for current company info

--- routers/return_db_contents.py
+++ routers/return_db_contents.py
@@ -8,32 +8,35 @@ from fastapi import Query
 router = APIRouter()
 
 @router.get("/retrieve-analysis")
-def fetch_analysis(ticker: str, timezone: str = Query(..., description="Timezone (e.g. 'Europe/Berlin')")):
+def fetch_analysis(ticker: str, only_database: bool = False, timezone: str = Query("UTC", description="Timezone (e.g. 'Europe/Berlin')")):
     with session_scope() as session:
         ticker_obj = session.query(Ticker).filter(func.lower(Ticker.symbol) == ticker.lower()).first()
 
         if not ticker_obj:
             return {"error": "Ticker not found"}
         
-        fmp_profile = get_stock_profile(ticker, timezone)
         company_data = {
             "ticker": ticker_obj.symbol,
             "title": ticker_obj.name,
             "description": ticker_obj.description,
             "sentimentScore": ticker_obj.overall_sentiment_score,
-            "logo": fmp_profile["logo"],
-            "website": fmp_profile["website"],
-            "price": fmp_profile["price"],
-            "exchangeShortName": fmp_profile["exchangeShortName"],
-            "mktCap": fmp_profile["mktCap"],
-            "industry": fmp_profile["industry"],
-            "earningsCallDate": fmp_profile["earningsCallDate"],
-            "analystRating": fmp_profile["analystRating"],
-            "forwardPE": fmp_profile["forwardPE"],
-            "dcf": fmp_profile["dcf"],
-            "beta": fmp_profile["beta"]
         }
 
+        if not only_database:
+            fmp_profile = get_stock_profile(ticker, timezone)
+            company_data.update({
+                "logo": fmp_profile["logo"],
+                "website": fmp_profile["website"],
+                "price": fmp_profile["price"],
+                "exchangeShortName": fmp_profile["exchangeShortName"],
+                "mktCap": fmp_profile["mktCap"],
+                "industry": fmp_profile["industry"],
+                "earningsCallDate": fmp_profile["earningsCallDate"],
+                "analystRating": fmp_profile["analystRating"],
+                "forwardPE": fmp_profile["forwardPE"],
+                "dcf": fmp_profile["dcf"],
+                "beta": fmp_profile["beta"]
+            })
 
         points_query = session.query(Point).filter(Point.ticker_id == ticker_obj.id).all()
         points_list = []
@@ -63,6 +66,3 @@ def fetch_analysis(ticker: str, timezone: str = Query(..., description="Timezone
             points_list.append(pt_data)
     
     return {"company": company_data, "points": points_list}
-
-        
-
